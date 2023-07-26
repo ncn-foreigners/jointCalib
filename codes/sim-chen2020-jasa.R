8c3f2f403e6220a79b58c698fc84933d880ffde1
Maciej <maciej.beresewicz@gmail.com>
2023-07-26 11:39:25 +0200

    final sim frm jasa

--- codes/sim-chen2020-jasa.R
+++ codes/sim-chen2020-jasa.R
@@ -16,6 +16,9 @@ seed_for_sim <- 2023-07-14
 set.seed(seed_for_sim+1)
 n_reps <- 1000
 
+
+# generate data according to the paper ------------------------------------
+
 N <- 20000
 n_1 <- 1000
 n_2 <- 1000
@@ -45,13 +48,9 @@ w_pA_1 <- 1/pA_1
 
 s_B <- stats::uniroot(f = function(s) max(s+x3)/min(s+x3) - 50, c(0, 100), tol = etol)$root
 pB_1 <- inclusionprobabilities(s_B + x3, n_1)
-pB_2 <- inclusionprobabilities(s_B + x3, n_2)
 w_pB_1 <- 1/pB_1
-w_pB_2 <- 1/pB_2
 
-pop_data <- data.frame(x1,x2,x3,x4,y1,y2,y3,pA_1,pA_2,w_pA_1,w_pA_2,pB_1,pB_2,w_pB_1,w_pB_2) |> setDT()
-
-
+pop_data <- data.frame(x1,x2,x3,x4,y1,y2,y3,pA_1,w_pA_1,pB_1,w_pB_1) |> setDT()
 
 
 # known population quantities ---------------------------------------------
@@ -89,22 +88,28 @@ results_y3_sample <-
   results_y2_sample <- 
   results_y1_sample <- matrix(data=0, 
                               nrow = n_reps, 
-                              ncol = (1+(k_quants_est+1))*4 + 1 + 3 + (k_quants_est+1)*2)
+                              ncol = (1+(k_quants_est+1))*4 + 1 + 3 + (k_quants_est+1)*3)
 
 
 
 for (r in 1:n_reps) {
   set.seed(r)
   if (r %% 50 == 0) cat("Iteration: ", r, "\n")
+  
+  ## nonprob sample
   sample_nonprob <- pop_data[which(sampling::UPpoisson(pop_data$pA_1)==1),]
+  sample_nonprob$w_naive <- N/n_1
+  
+  ## probability sample
   sample_prob <- pop_data[syspps_cpp(pop_data$pB_1, n_1), ] 
-  sample_nonprob$w_naive <- sum(1/sample_prob$pB_1)/n_1
   sample_prob_svy <- svydesign(ids=~1, probs =~pB_1, data = sample_prob)
   
-  q_est <- svyquantile( ~ x2 + x3 + x4, sample_prob_svy, p_quantiles)
-  x_totals <- svytotal( ~ x1 + x2 + x3 + x4, sample_prob_svy) |> as.numeric()
+  ## estimated population size, totals and quantiles
   N_hat <- sum(weights(sample_prob_svy))
+  x_totals <- svytotal( ~ x1 + x2 + x3 + x4, sample_prob_svy) |> as.numeric()
+  q_est <- svyquantile( ~ x2 + x3 + x4, sample_prob_svy, p_quantiles)
   
+  ## IPW
   ipw_y1 <- nonprob(selection = ~ x1 + x2 + x3 + x4,
                     target = ~ y1,
                     svydesign = sample_prob_svy,
@@ -120,6 +125,7 @@ for (r in 1:n_reps) {
                     svydesign = sample_prob_svy,
                     data = sample_nonprob)
   
+  ## MI based on model
   mi_y1 <- nonprob(outcome = y1 ~ x1 + x2 + x3 + x4,
                    svydesign = sample_prob_svy,
                    data = sample_nonprob)
@@ -131,8 +137,9 @@ for (r in 1:n_reps) {
   mi_y3 <- nonprob(outcome = y3 ~ x1 + x2 + x3 + x4,
                    svydesign = sample_prob_svy,
                    data = sample_nonprob)
+
   
-  
+  ## DR
   dr_y1 <- nonprob(selection = ~ x1 + x2 + x3 + x4,
                    outcome = y1 ~ x1 + x2 + x3 + x4,
                    svydesign = sample_prob_svy,
@@ -148,6 +155,7 @@ for (r in 1:n_reps) {
                    svydesign = sample_prob_svy,
                    data = sample_nonprob)
   
+  ## calibration with quantiles only
   w_res <- calib_quantiles(X_q = with(sample_nonprob, cbind(x2, x3, x4)),  
                            d = sample_nonprob$w_naive,  
                            N=N_hat,  
@@ -155,7 +163,7 @@ for (r in 1:n_reps) {
                            method = "raking",
                            backend = "sampling")
   
-  
+  ## calibration with quantiles and totals
   w_res2 <- calib_quantiles(X_q = with(sample_nonprob, cbind(x2, x3, x4)),
                             X =  with(sample_nonprob, cbind(x1, x2, x3, x4)),
                             d = sample_nonprob$w_naive,
@@ -165,11 +173,13 @@ for (r in 1:n_reps) {
                             method = "raking",
                             backend = "sampling")
   
+  ## calibration with totals only
   w_res_st <- calib(Xs = with(sample_nonprob, cbind(x1, x2, x3, x4)), 
                     d = sample_nonprob$w_naive, 
                     total = x_totals, 
                     method = "raking")
   
+  ## weights
   sample_nonprob$w_cal_q1 <- w_res$w
   sample_nonprob$w_cal_q2 <- w_res2$w
   sample_nonprob$w_cal_s <- sample_nonprob$w_naive*w_res_st
@@ -186,10 +196,11 @@ for (r in 1:n_reps) {
   results_y1_sample[r, 8] <- dr_y1$output$mean
   results_y1_sample[r, 9:(9+k_quants_est)] <- with(sample_nonprob, quantile(y1, p_quantiles_est))
   results_y1_sample[r, (9+1*k_quants_est+1):(9+2*k_quants_est+1)] <- with(sample_nonprob, weightedQuantile(y1, w_ipw,p_quantiles_est))
-  results_y1_sample[r, (9+2*k_quants_est+2):(9+3*k_quants_est+2)] <- with(sample_nonprob, weightedQuantile(y1, w_dr,p_quantiles_est))
-  results_y1_sample[r, (9+3*k_quants_est+3):(9+4*k_quants_est+3)] <- with(sample_nonprob, weightedQuantile(y1, w_cal_s, p_quantiles_est))
-  results_y1_sample[r, (9+4*k_quants_est+4):(9+5*k_quants_est+4)] <- with(sample_nonprob, weightedQuantile(y1, w_cal_q1, p_quantiles_est))
-  results_y1_sample[r, (9+5*k_quants_est+5):(9+6*k_quants_est+5)] <- with(sample_nonprob, weightedQuantile(y1, w_cal_q2, p_quantiles_est))
+  results_y1_sample[r, (9+2*k_quants_est+2):(9+3*k_quants_est+2)] <- quantile(mi_y1$outcome$glm$fitted.values, p_quantiles_est)
+  results_y1_sample[r, (9+3*k_quants_est+3):(9+4*k_quants_est+3)] <- with(sample_nonprob, weightedQuantile(y1, w_dr,p_quantiles_est))
+  results_y1_sample[r, (9+4*k_quants_est+4):(9+5*k_quants_est+4)] <- with(sample_nonprob, weightedQuantile(y1, w_cal_s, p_quantiles_est))
+  results_y1_sample[r, (9+5*k_quants_est+5):(9+6*k_quants_est+5)] <- with(sample_nonprob, weightedQuantile(y1, w_cal_q1, p_quantiles_est))
+  results_y1_sample[r, (9+6*k_quants_est+6):(9+7*k_quants_est+6)] <- with(sample_nonprob, weightedQuantile(y1, w_cal_q2, p_quantiles_est))
   
   results_y2_sample[r, 1] <- r
   results_y2_sample[r, 2] <- with(sample_nonprob, mean(y2))
@@ -201,10 +212,11 @@ for (r in 1:n_reps) {
   results_y2_sample[r, 8] <- dr_y2$output$mean
   results_y2_sample[r, 9:(9+k_quants_est)] <- with(sample_nonprob, quantile(y2, p_quantiles_est))
   results_y2_sample[r, (9+1*k_quants_est+1):(9+2*k_quants_est+1)] <- with(sample_nonprob, weightedQuantile(y2, w_ipw,p_quantiles_est))
-  results_y2_sample[r, (9+2*k_quants_est+2):(9+3*k_quants_est+2)] <- with(sample_nonprob, weightedQuantile(y2, w_dr,p_quantiles_est))
-  results_y2_sample[r, (9+3*k_quants_est+3):(9+4*k_quants_est+3)] <- with(sample_nonprob, weightedQuantile(y2, w_cal_s, p_quantiles_est))
-  results_y2_sample[r, (9+4*k_quants_est+4):(9+5*k_quants_est+4)] <- with(sample_nonprob, weightedQuantile(y2, w_cal_q1, p_quantiles_est))
-  results_y2_sample[r, (9+5*k_quants_est+5):(9+6*k_quants_est+5)] <- with(sample_nonprob, weightedQuantile(y2, w_cal_q2, p_quantiles_est))
+  results_y2_sample[r, (9+2*k_quants_est+2):(9+3*k_quants_est+2)] <- quantile(mi_y2$outcome$glm$fitted.values, p_quantiles_est)
+  results_y2_sample[r, (9+3*k_quants_est+3):(9+4*k_quants_est+3)] <- with(sample_nonprob, weightedQuantile(y2, w_dr,p_quantiles_est))
+  results_y2_sample[r, (9+4*k_quants_est+4):(9+5*k_quants_est+4)] <- with(sample_nonprob, weightedQuantile(y2, w_cal_s, p_quantiles_est))
+  results_y2_sample[r, (9+5*k_quants_est+5):(9+6*k_quants_est+5)] <- with(sample_nonprob, weightedQuantile(y2, w_cal_q1, p_quantiles_est))
+  results_y2_sample[r, (9+6*k_quants_est+6):(9+7*k_quants_est+6)] <- with(sample_nonprob, weightedQuantile(y2, w_cal_q2, p_quantiles_est))
   
   results_y3_sample[r, 1] <- r
   results_y3_sample[r, 2] <- with(sample_nonprob, mean(y3))
@@ -216,27 +228,29 @@ for (r in 1:n_reps) {
   results_y3_sample[r, 8] <- dr_y3$output$mean
   results_y3_sample[r, 9:(9+k_quants_est)] <- with(sample_nonprob, quantile(y3, p_quantiles_est))
   results_y3_sample[r, (9+1*k_quants_est+1):(9+2*k_quants_est+1)] <- with(sample_nonprob, weightedQuantile(y3, w_ipw,p_quantiles_est))
-  results_y3_sample[r, (9+2*k_quants_est+2):(9+3*k_quants_est+2)] <- with(sample_nonprob, weightedQuantile(y3, w_dr,p_quantiles_est))
-  results_y3_sample[r, (9+3*k_quants_est+3):(9+4*k_quants_est+3)] <- with(sample_nonprob, weightedQuantile(y3, w_cal_s, p_quantiles_est))
-  results_y3_sample[r, (9+4*k_quants_est+4):(9+5*k_quants_est+4)] <- with(sample_nonprob, weightedQuantile(y3, w_cal_q1, p_quantiles_est))
-  results_y3_sample[r, (9+5*k_quants_est+5):(9+6*k_quants_est+5)] <- with(sample_nonprob, weightedQuantile(y3, w_cal_q2, p_quantiles_est))
+  results_y3_sample[r, (9+2*k_quants_est+2):(9+3*k_quants_est+2)] <- quantile(mi_y3$outcome$glm$fitted.values, p_quantiles_est)
+  results_y3_sample[r, (9+3*k_quants_est+3):(9+4*k_quants_est+3)] <- with(sample_nonprob, weightedQuantile(y3, w_dr,p_quantiles_est))
+  results_y3_sample[r, (9+4*k_quants_est+4):(9+5*k_quants_est+4)] <- with(sample_nonprob, weightedQuantile(y3, w_cal_s, p_quantiles_est))
+  results_y3_sample[r, (9+5*k_quants_est+5):(9+6*k_quants_est+5)] <- with(sample_nonprob, weightedQuantile(y3, w_cal_q1, p_quantiles_est))
+  results_y3_sample[r, (9+6*k_quants_est+6):(9+7*k_quants_est+6)] <- with(sample_nonprob, weightedQuantile(y3, w_cal_q2, p_quantiles_est))
   
 }
 
 
 results_sample <- rbind(results_y1_sample, results_y2_sample, results_y3_sample) |> as.data.frame() |> setDT()
+
 setnames(results_sample, names(results_sample), 
          c("iter", "m_naive", "m_cal", "m_qcal1", "m_qcal2",
            "m_ipw", "m_mi", "m_dr",
            paste(paste0("q", rep(c(25, 50, 75), times = 4)), 
-                 rep(c("naive", "cal", "ipw", "dr", "qcal1", "qcal2"), each = 3), sep="_")))
+                 rep(c("naive", "ipw", "mi", "dr", "cal", "qcal1", "qcal2"), each = 3), sep="_")))
 
 results_sample[, y:=rep(c("y1", "y2", "y3"), each = n_reps)]
 results_sample <- melt(results_sample, id.vars = c("iter", "y"), value.name = "value", variable.name = "estimator") 
 results_sample[, c("estimator", "type") := tstrsplit(estimator, "_")]
 results_sample <- results_sample[pop_true_vals, on = c("y", "estimator")]
-results_sample[, type:= factor(type, c("naive", "cal", "ipw", "mi", "dr", "qcal1", "qcal2"),
-                               c("Naive", "Cal", "IPW", "MI", "DR", "QCal1", "QCal2"))]
+results_sample[, type:= factor(type, c("naive", "ipw", "mi", "dr", "cal", "qcal1", "qcal2"),
+                               c("Naive", "IPW", "MI", "DR", "Cal", "QCal1", "QCal2"))]
 results_sample[, estimator:= factor(estimator, c("m", "q25", "q50", "q75"), c("Mean", "Q25", "Q50", "Q75"))]
 
 
